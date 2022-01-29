e96cf4ac7c70effafc53e8c838d6fafce5ce39f8
Selawen <37039183+Selawen@users.noreply.github.com>
2022-01-29 13:48:21 +0100

    got notes working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,27 +5,57 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<Note> notes;
-    private Color[] noteColours;
-    [SerializeField] private int playercount;
+    [SerializeField] private List<Note> notepool;
+    private string[] noteColours;
+    private KeyCode[] noteDirections;
+    [SerializeField] [Range(1,4)] private int playercount;
 
     [SerializeField] private int bpm;
-    private float timer = 0;
+    private float beatTime;
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
+        beatTime = 60f / bpm;
         notes = new List<Note>();
-        noteColours = new Color[4]{Color.red, Color.blue, Color.green, Color.yellow };
+        notepool = new List<Note>();
+        noteColours = new string[4]{"red", "blue", "green", "yellow" };
+        noteDirections = new KeyCode[4]{KeyCode.A, KeyCode.W, KeyCode.S, KeyCode.D };
         ResetTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(notes.Count >0)
+        {
+            if (notes[0].DestroyNote()){
+                notepool.Add(notes[0]);
+                notes.RemoveAt(0);
+                Debug.Log("note destroyed");
+            }
+        }
+
+        foreach (Note n in notes){
+            n.MoveNotes(beatTime);
+        }
+
         timer -= Time.deltaTime;
         if (timer <= 0)
         {
-            notes.Add(new Note(noteColours[(int)Random.Range(0, playercount+0.9f)]));
+            int pos = (int)Random.Range(0, 4-0.1f);
+            
+            if (notepool.Count <1)
+            {
+            GameObject note = Instantiate(Resources.Load<GameObject>("Prefabs/Note"), new Vector3(pos*4, 5.5f, 0), transform.rotation);            
+            notes.Add(new Note(note, noteColours[(int)Random.Range(0, playercount-0.1f)], noteDirections[pos]));
+            } else {
+                //GameObject note = Instantiate(Resources.Load<GameObject>("Prefabs/Note"), new Vector3(pos*4, 2, 0), transform.rotation);
+                notepool[0].ResetNote(noteColours[(int)Random.Range(0, playercount-0.1f)], noteDirections[pos], new Vector3(pos*4, 5.5f, 0));    
+                notes.Add(notepool[0]);
+                notepool.RemoveAt(0);
+            }
             ResetTimer();
         }
     }
@@ -33,6 +63,7 @@ public class GameManager : MonoBehaviour
     private void ResetTimer()
     {
         timer = (int)Random.Range(1, 10);
-        timer *= 60 / bpm;
+        timer *= beatTime;
+        //Debug.Log(timer);
     }
 }
--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -4,31 +4,51 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
-    [SerializeField] private Color noteColour;
-    [SerializeField] private GameObject notePrefab;
+    [SerializeField] private string noteColour;
+    private KeyCode key;
+    //[SerializeField] private GameObject notePrefab;
     private GameObject note;
-    Vector3 down = new Vector3(0, -1, 0);
+    Vector3 down = new Vector3(0, -0.01f, 0);
 
-    public Note(Color colour)
+    public Note(GameObject _note, string _colour, KeyCode _key)
     {
-        noteColour = colour;
-        note = Instantiate(notePrefab);
+        noteColour = _colour;
+        note = _note;
+        key = _key;
+        //Debug.Log("Materials/" + _colour);
+        //Debug.Log(Resources.Load<Material>("Materials/" + _colour));
+        note.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/" + noteColour);
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        //notePrefab = Resources.Load<GameObject>("Prefabs/Note");
+    }
+
+    public void ResetNote(string _colour, KeyCode _key, Vector3 _position)
+    {
+        noteColour = _colour;
+        key = _key;
+        note.transform.position = _position;
+        note.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/" + noteColour);
+    }
+    // Update is called once per frame
+    public void MoveNotes(float bpmMultiplier)
+    {
+        note.transform.Translate(down/bpmMultiplier);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        note.transform.Translate(down);
-        if (note.transform.position.y <= -5)
+    public bool DestroyNote(){
+    if (note.transform.position.y <= -5.5f)
         {
-            //ToDo: make objectpool
-            Destroy(this);
-        }
+            return true;        
+        } else {return false;}
+    }
+
+    public bool Hit(KeyCode _key, string _colour)
+    {
+        return (_key == key && _colour == noteColour);
     }
 }
